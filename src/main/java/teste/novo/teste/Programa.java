e4624d342aef2a132a80cc07053e71dec5146d7d
Breno-SO <Breno.soliv@gmail.com>
2021-02-18 15:27:54 -0300

    mensagem de boa tarde e noite

--- src/main/java/teste/novo/teste/Programa.java
+++ src/main/java/teste/novo/teste/Programa.java
@@ -7,6 +7,8 @@ package teste.novo.teste;
 public class Programa {
     public static void main(String[] args) {
         System.out.println("Bom dia mundo");
+        System.out.println("Boa tarde mundo");
+        System.out.println("Boa noite mundo");
     }
     
 }
